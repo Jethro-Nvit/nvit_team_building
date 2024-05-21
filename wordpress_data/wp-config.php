c6ac18c9965a9aad2abfccc1fd5991867c5452ba
Jethro-Nvit <160114839+Jethro-Nvit@users.noreply.github.com>
2024-05-21 14:48:01 +0100

    Update wp-config.php

--- wordpress_data/wp-config.php
+++ wordpress_data/wp-config.php
@@ -131,3 +131,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
+//define('WP_HOME', 'http://new-ip-or-domain');
+//define('WP_SITEURL', 'http://new-ip-or-domain');
